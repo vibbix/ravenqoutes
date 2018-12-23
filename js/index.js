4ee1088bfe39f0aea04233895ae54aa05bf5118b
Mark Beznos <mark.vibbix@gmail.com>
2018-12-22 20:09:04 -0500

    more stuff

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-var markactions = ["touch my inner thigh", "forgot my name", "sad spank me", "look sad", "build a snowman", "make me a snowman", "fall asleep on me again", "play dark drone ambient banjo music", "lick things", "go hang out with Cara", "let your phone died again", "get lost in a circle", "get lost in the mountain that is the stuff on my floor", "drive so fast", "try grabbing the pen"];
-var ravenactions = ["playing the banjo", "reading", "crying about not getting my milkshake", "carried from the hospital", "being Raven", "avoiding El Segundo", "buying your sister a backpack", "listening to Oingo Boingo", "becoming Ezekiel Goldthwait", "admiring Jemima Debuke", "being a funny person", "watching crappy Christmas movies"];
-let qoutetypes = 1;
+var markactions = ["touch my inner thigh", "forgot my name", "sad spank me",
+    "look sad", "build a snowman", "make me a snowman", "fall asleep on me again",
+    "play dark drone ambient banjo music", "lick things", "go hang out with Cara",
+    "let your phone die again", "get lost in a circle", "make this website",
+    "get lost in the mountain that is the stuff on my floor", "drive so fast",
+    "try grabbing the pen", "space out", "start a fire with all the string lights",
+    "like candles a bit too much", "steal more street signs", "go to my improv show"];
+var ravenactions = ["playing the banjo", "reading", "crying about not getting my milkshake",
+    "carried home from the hospital", "being Raven", "avoiding El Segundo", "buying your sister a backpack",
+    "listening to Oingo Boingo", "becoming Ezekiel Goldthwait", "admiring Jemima Debuke", "being a funny person",
+    "watching crappy Christmas movies", "getting broken up with over email", "wearing my Nicolas Cage shirt", "12",
+    "making Lord of The Rings references", "at the Struts concert with you", "at the work party with you",
+    "trying to look bothered", "doing art student things"
+];
+let qoutetypes = 100;
 
 function getRandomListItem(list) {
     var i = Math.floor(Math.random() * list.length);
@@ -9,12 +21,12 @@ function getRandomListItem(list) {
 
 function generateQoute() {
     var qoutetype = Math.floor(Math.random() * qoutetypes);
-    if (qoutetype == 0) {
+    if (qoutetype < 99) {
         var markAction = getRandomListItem(markactions);
         var ravenAction = getRandomListItem(ravenactions);
         return `Don't ${markAction} while I'm ${ravenAction}.`;
     }
-    return "";
+    return "I love you.";
 }
 
 function setQoute() {
