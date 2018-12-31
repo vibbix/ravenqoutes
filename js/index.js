213b29aaf6b5ed3e94fa8b01f31dbd3c21ad31c7
Mark Beznos <mark.vibbix@gmail.com>
2018-12-31 17:09:49 -0500

    more qoutes

--- js/index.js
+++ js/index.js
@@ -13,20 +13,21 @@ let analytics = 'UA-131324802-1';
 ga('create', analytics, 'auto');
 ga('send', 'pageview');
 
-var markactions = ["touch my inner thigh", "forgot my name", "sad spank me",
+var markactions = ["touch my inner thigh", "forgot my name", "sad spank me", "call me at 3AM again",
     "look sad", "build a snowman", "make me a snowman", "fall asleep on me again", "dangle off the rooftop",
-    "play dark drone ambient banjo music", "lick things", "go hang out with Cara",
-    "let your phone die again", "get lost in a circle", "make this website", "be born of incest",
+    "play dark drone ambient banjo music", "lick things", "go hang out with Cara", "dirty talk in Russian",
+    "let your phone die again", "get lost in a circle", "make this website", "be born of incest", "make bad decisions 9 months before you knew me", 
     "get lost in the mountain that is the stuff on my floor", "drive so fast", "forget to eat because JD isn't cooking",
-    "try grabbing the pen", "space out", "start a fire with all the string lights",
-    "like candles a bit too much", "steal more street signs", "go to my improv show"
+    "try grabbing the pen", "space out", "start a fire with all the string lights", "disappoint me",
+    "like candles a bit too much", "steal more street signs", "go to my improv show", "be my favorite"
 ];
-var ravenactions = ["playing the banjo", "reading", "crying about not getting my milkshake",
+var ravenactions = ["playing the banjo", "reading", "crying about not getting my milkshake", "trying to dirty talk in Spanish",
     "carried home from the hospital", "being Raven", "avoiding El Segundo", "buying your sister a backpack",
     "listening to Oingo Boingo", "becoming Ezekiel Goldthwait", "admiring Jemima Debuke", "being a funny person",
     "watching crappy Christmas movies", "getting broken up with over email", "wearing my Nicolas Cage shirt", "12",
     "making Lord of The Rings references", "at the Struts concert with you", "at the work party with you",
-    "trying to look bothered", "doing art student things", "born of incest", "having scary dreams about your ex"
+    "trying to look bothered", "doing art student things", "born of incest", "having scary dreams about your ex",
+    "trying to be vegan", "being your favorite"
 ];
 let qoutetypes = 100;
 let version = "v1.2"
