31cc5a3e65e975f5b58e19e6ba854039c0f591a3
Mark Beznos <mark.vibbix@gmail.com>
2018-12-31 09:24:32 -0500

    Updated event sending

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
+let analytics = 'UA-131324802-1';
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', analytics, 'auto');
+ga('send', 'pageview');
+
 var markactions = ["touch my inner thigh", "forgot my name", "sad spank me",
     "look sad", "build a snowman", "make me a snowman", "fall asleep on me again",
     "play dark drone ambient banjo music", "lick things", "go hang out with Cara",
     "let your phone die again", "get lost in a circle", "make this website", "be born of incest",
     "get lost in the mountain that is the stuff on my floor", "drive so fast", "forget to eat because JD isn't cooking",
     "try grabbing the pen", "space out", "start a fire with all the string lights",
-    "like candles a bit too much", "steal more street signs", "go to my improv show"];
+    "like candles a bit too much", "steal more street signs", "go to my improv show"
+];
 var ravenactions = ["playing the banjo", "reading", "crying about not getting my milkshake",
     "carried home from the hospital", "being Raven", "avoiding El Segundo", "buying your sister a backpack",
     "listening to Oingo Boingo", "becoming Ezekiel Goldthwait", "admiring Jemima Debuke", "being a funny person",
@@ -29,9 +45,16 @@ function generateQoute() {
     return "I love you.";
 }
 
-function setQoute() {
+function setQoute(sendevent = true) {
     var qoute = generateQoute();
     document.getElementById("qoute").innerHTML = qoute;
+    if (sendevent) {
+        ga('send', 'event', 'generate', 'generateqoute');
+    }
+    console.log(`Qoute generated: ${qoute}; Event: ${sendevent}`)
+
 }
 
-setQoute();
+function initPage() {
+    setQoute(false);
+}
