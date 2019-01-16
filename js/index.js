4dcf72caf109c6e94dbb0ba908b345421fea1934
Mark Beznos <mark.vibbix@gmail.com>
2019-01-16 08:18:22 -0500

    - moar RAVEN

--- js/index.js
+++ js/index.js
@@ -16,10 +16,10 @@ ga('send', 'pageview');
 var markactions = ["touch my inner thigh", "forgot my name", "sad spank me", "call me at 3AM again",
     "look sad", "build a snowman", "make me a snowman", "fall asleep on me again", "dangle off the rooftop",
     "play dark drone ambient banjo music", "lick things", "go hang out with Cara", "dirty talk in Russian",
-    "let your phone die again", "get lost in a circle", "make this website", "be born of incest", "make bad decisions 9 months before you knew me", 
-    "get lost in the mountain that is the stuff on my floor", "drive so fast", "forget to eat because JD isn't cooking",
-    "try grabbing the pen", "space out", "start a fire with all the string lights", "disappoint me",
-    "like candles a bit too much", "steal more street signs", "go to my improv show", "be my favorite"
+    "let your phone die again", "get lost in a circle", "make this website", "be born of incest", "manage to mispell quotes",
+    "get lost in the mountain that is the stuff on my floor", "drive so fast", "remove bad qoutes from the site",
+    "try grabbing the pen", "space out", "start a fire with all the string lights", "disappoint me", "visit the clinic",
+    "like candles a bit too much", "steal more street signs", "go to my improv show", "be my favorite", "taste the excellence"
 ];
 var ravenactions = ["playing the banjo", "reading", "crying about not getting my milkshake", "trying to dirty talk in Spanish",
     "carried home from the hospital", "being Raven", "avoiding El Segundo", "buying your sister a backpack",
@@ -27,10 +27,12 @@ var ravenactions = ["playing the banjo", "reading", "crying about not getting my
     "watching crappy Christmas movies", "getting broken up with over email", "wearing my Nicolas Cage shirt", "12",
     "making Lord of The Rings references", "at the Struts concert with you", "at the work party with you",
     "trying to look bothered", "doing art student things", "born of incest", "having scary dreams about your ex",
-    "trying to be vegan", "being your favorite"
+    "trying to be vegan", "being your favorite", "trying to show up my Ex during NYE", "eat my children", "talk about my chexmix"
 ];
 let qoutetypes = 100;
 let version = "v1.2"
+
+"I can'r"
 
 function getRandomListItem(list) {
     var i = Math.floor(Math.random() * list.length);
@@ -43,7 +45,7 @@ function generateQoute() {
         var markAction = getRandomListItem(markactions);
         var ravenAction = getRandomListItem(ravenactions);
         return `Don't ${markAction} while I'm ${ravenAction}.`;
-    }
+    } 
     return "I love you.";
 }
 
